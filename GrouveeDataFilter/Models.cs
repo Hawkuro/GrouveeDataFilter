c63c1ac5838908f9971694e0de43f161eee8096d
Haukur Óskar Þorgeirsson <haukuroskar@meniga.com>
2018-05-08 23:05:24 +0000

    Models starting to take shape

---
+++ GrouveeDataFilter/Models.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileHelpers;
+
+namespace GrouveeDataFilter.Models
+{
+    [DelimitedRecord(","), IgnoreFirst]
+    public class GrouveeGame
+    {
+        public enum LevelOfCompletion
+        {
+            [Description("Main Story")]
+            MainStory,
+            [Description("Main Story + Extras")]
+            MainStoryExtras,
+            [Description("100% Completion")]
+            HundredPercent
+        }
+
+        public class DateData
+        {
+            public DateTime? date_finished;
+            public DateTime? date_started;
+            public LevelOfCompletion level_of_completion;
+            public int seconds_played;
+        }
+
+        public class Shelf
+        {
+            public string name;
+            public Uri url;
+            public DateTime date_added;
+            public int order;
+        }
+        public class Platform
+        {
+            public string name;
+            public Uri url;
+        }
+
+        public int id;
+        public string name;
+        [FieldQuoted] //Shelf
+        public string shelves;
+        [FieldQuoted] //Platform
+        public string platforms;
+        public int? rating;
+        public string review;
+        [FieldQuoted] //DateData
+        public string dates;
+
+    }
+
+}
---
+++ GrouveeDataFilter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileHelpers;
+using GrouveeDataFilter.Models;
+
+namespace GrouveeDataFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var grouveeDataFileName = args[0];
+            var engine = new FileHelperEngine<GrouveeGame>(Encoding.UTF8);
+            engine.ReadFile("C:\\Users\\haukuroskar\\Downloads\\Hawkuro_26904_grouvee_export.csv");
+        }
+    }
+}
